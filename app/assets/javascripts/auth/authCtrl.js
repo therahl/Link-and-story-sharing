a0b4ac1a61a08a7758b34941b2dee7cb42fe2b71
Jay Hennessy <JAYHENNESSY@GMAIL.COM>
2015-07-16 14:14:40 -0700

    built devise forms

---
+++ app/assets/javascripts/auth/authCtrl.js
@@ -0,0 +1,17 @@
+myApp.controller('AuthCtrl', [
+  '$scope',
+  '$state',
+  'Aith',
+  function($scope, $state, Auth){
+    $scope.login = function(){
+      Auth.login($scope.user).then(function(){
+        $state.go('home');
+      });
+    };
+    $scope.register = function(){
+      Auth.register($scope.user).then(function(){
+        $state.go('home');
+      });
+    };
+  }
+])
