ef986798781e198c381006c8309faed42d8e94b1
therahl <skydropsblue@gmail.com>
2015-07-20 17:38:56 -0700

    added seed file using faker

--- Gemfile
+++ Gemfile
@@ -25,7 +25,7 @@ gem 'angular_rails_csrf'
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem 'byebug'
-
+  gem 'faker'
   # Access an IRB console on exception pages or by using <%= console %> in views
   gem 'web-console', '~> 2.0'
 
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,28 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+10.times do
+  username = Faker::Internet.user_name
+  email = Faker::Internet.free_email
+  password = 'password'
+  user = User.create(username: username, email: email, password: password, password_confirmation: password)
+end
+
+10.times do
+  users = User.all.to_a.shuffle
+  user = users.shift
+  title = Faker::Lorem.sentence
+  description = Faker::Lorem.paragraph(5,true,4)
+  upvotes = rand(1..25)
+  post = Post.create(upvotes: upvotes, title: title, description: description, user_id: user.id)
+end
+
+Post.all.each do |post|
+    users = User.all.shuffle
+  rand(3..7).times do
+    user = users.shift
+    body = Faker::Hacker.say_something_smart
+    upvotes = rand(1..25)
+    comment = post.comments.create(body: body, user_id: user.id, upvotes: upvotes)
+  end
+end
